508988c49eb3352e8e992617ed9c5bb742c4c5e7
Tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2017-07-10 17:03:55 +1200

    fixes

--- cu/database/models.py
+++ cu/database/models.py
@@ -11,10 +11,15 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
+    password = Column(Text)
 
     def __init__(self, username=None, email=None):
         self.username = username
         self.email = email
+
+    def verify_password(self, attempted):
+        pwhash = bcrypt.hashpw(attempted, self.password)
+        return self.password == pwhash
 
     def __repr__(self):
         return '<User %r: %r>' % (self.id, self.username)
@@ -26,6 +31,12 @@ class Organisation(Base):
     name = Column(String, unique=True, nullable=False)
     password = Column(Text)
 
+    def __init__(self, name, password):
+        self.name = name
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
+
     def verify_password(self, attempted):
-        pwhash = bcrypt.hashpw(attempted, self.password)
-        return self.password == pwhash
+        return bcrypt.checkpw(attempted, self.password)
+
+    def __repr__(self):
+        return '<Organisation %r: %r>' % (self.id, self.name)
